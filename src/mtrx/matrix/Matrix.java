8985a517e63094b58874354419fe2a02b80bfee3
ravielze <ravielz20@gmail.com>
2020-09-23 01:35:02 +0700

    Fix conflict

--- src/mtrx/matrix/Matrix.java
+++ src/mtrx/matrix/Matrix.java
@@ -53,11 +53,7 @@ public class Matrix implements IMatrix {
 
     @Override
     public void swapCol(int colA, int colB) {
-<<<<<<< HEAD
         double[] tempA = new double[this.row], tempB = new double[this.row];
-=======
-        final double[] tempA = new double[this.row];
->>>>>>> e402db6525cdeeac954f9700ff451598ca5a4de7
         for (int i = 0; i < this.row; i++) {
             tempA[i] = this.data[i][colA];
             this.data[i][colA] = this.data[i][colB];
