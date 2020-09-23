d1b7af2a02e90651dfa0fb02cfd7a699a1df1ea7
ravielze <ravielz20@gmail.com>
2020-09-23 17:26:11 +0700

    Update MatrixTrait

--- src/mtrx/trait/MatrixTrait.java
+++ src/mtrx/trait/MatrixTrait.java
@@ -6,26 +6,6 @@ public enum MatrixTrait {
      * Matrix yang memiliki ukuran NxN
      */
     SQUARE,
-
-    /**
-     * Matriks persegi yang di atas garis diagonal utama nol
-     */
-    UPPER_TRIANGLE,
-
-    /**
-     * Matriks persegi yang di bawah garis diagonal utama nol
-     */
-    LOWER_TRIANGLE,
-
-    /**
-     * Matriks eselon
-     */
-    ESELON,
-
-    /**
-     * Alias untuk Eselon Baris terreduksi
-     */
-    ESELON_R,
     
     /**
      * Matriks Identitas.
