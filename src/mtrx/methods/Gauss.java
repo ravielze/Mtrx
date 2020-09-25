40a83e450e7e7951b10bc1b8129be3d80989ade6
cyn-rus <cyn.rusadi@gmail.com>
2020-09-26 02:58:33 +0700

    gauss ;-;

--- src/mtrx/methods/Gauss.java
+++ src/mtrx/methods/Gauss.java
@@ -22,7 +22,91 @@ public class Gauss implements MatrixMethod{
     }
 
 	private void operate() {
-        for (int i = 0; i < this.matrix.getColCount(); )
+        int i, l, row, j = 0, kValid = 0, bValid = 0;
+        boolean found, valid;
+        double val;
+
+        // buat nuker2
+        for (i = 0; i < this.matrix.getRowCount(); i++) {
+            found = false;
+
+            //ini jaga-jaga aja punten
+        // while (i < this.matrix.getRowCount() && swapped != 2) {
+            // if (this.matrix.getElement(0, 0) == 0 && this.matrix.getElement(i, 0) != 0) {
+            //     this.swapTimes++;
+            //     swapped++;
+            //     if (this.matrix.findCol(0, 1)) row = this.matrix.findFirstXinCol(0, 1);
+            //     else row = i;
+            //     this.matrix.swapRow(0, row);
+                while (j < this.matrix.getRowCount() && !found) {
+            // for (j = 0; j < this.matrix.getRowCount(); j++) {
+                if (this.matrix.getElement(i, j) == 0 && i != j) {
+                    for (l = i+1; l < this.matrix.getRowCount(); l++) {
+                        if (this.matrix.getElement(l, j) != 0) {
+                            found = true;
+                            this.swapTimes++;
+                            if (this.matrix.findCol(1, 1)) row = this.matrix.findFirstXinCol(1, 1);
+                            else row = l;
+                            this.matrix.swapRow(i, row);
+                        }
+                    }
+                }
+                j++;
+            }
+        }
+
+        for (i = 0; i < this.matrix.getRowCount(); i++) {
+            found = false;
+            j = 0;
+
+            while (j < this.matrix.getRowCount() && !found) {
+                if (this.matrix.getElement(i, j) != 0) found = true;
+                else j++;
+            }
+
+            if (i == 0) this.matrix.rowOperation(0, (x, y) -> x/(this.matrix.getElement(0, 0)));
+            else {
+                if (this.matrix.getElement(i-1, j) == 0) {
+                    val = this.matrix.getElement(i, j);
+                    this.matrix.rowOperation(i, j, (x, y) -> x / val);
+                }
+                else {
+                    val = this.matrix.getElement(i, j) / this.matrix.getElement(i-1, j);
+                    this.matrix.rowOperation(i, i-1, (x, y) -> x - val*(i-1));
+                }
+            }
+        }
+
+        i = this.matrix.getRowCount()-1;
+        while (i >= 0 && !this.hasSolution) {
+        // for (i = this.matrix.getRowCount()-1; i >= 0; i--) {
+            kValid = 0;
+            j = 0;
+            valid = false;
+            while (j < this.matrix.getColCount() && !valid) {
+                if (this.matrix.getElement(i, j) != 0 && j != this.matrix.getColCount()-1) {
+                    this.hasSolution = true;
+                    valid = true;
+                }
+
+                if (this.matrix.getElement(i, j) == 0) {
+                    kValid++;
+                    if (kValid == this.matrix.getColCount()) {
+                        bValid++;
+                    }
+                    if (kValid == this.matrix.getColCount()-1 &&
+                    this.matrix.getElement(i, this.matrix.getColCount()-1) != 0) {
+                        this.hasSolution = false;
+                    }
+                }
+                
+                if (kValid == this.matrix.getRowCount()) {
+                    this.hasSolution = false;
+                }
+                j++;
+            }
+            i--;
+        }
     }
     /**
      * 1 2 3
