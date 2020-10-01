af78cb599b63fa73fdb9e614f9990631ba5a9934
ravielze <ravielz20@gmail.com>
2020-10-02 00:37:02 +0700

    Fixx Gauss & GaussJordan

--- src/mtrx/methods/Gauss.java
+++ src/mtrx/methods/Gauss.java
@@ -25,6 +25,9 @@ public class Gauss implements GaussMethod {
             if (this.result.isAllXinCol(j, 0.0D) && (!this.result.isAllXinCol(j+1, 0.0D))){
                 col = j+1;
                 break;
+            } else if (!this.result.isAllXinCol(j, 0.0D)){
+                col = j;
+                break;
             }
         }
         if (col > this.result.getLeft().getColCount() || colZero == this.result.getLeft().getColCount()){
@@ -34,8 +37,12 @@ public class Gauss implements GaussMethod {
         for (int i = 0; i < this.result.getRowCount(); i++){
             this.result.fixZeroRow(i);
         }
-
+        int number =0;
         while (true){
+            number++;
+            System.out.printf("[%d]\n", number);
+            this.result.show(false);
+            System.out.println();
             if (NUtils.ISEQUAL(this.result.getLeft().getElement(row, col), 0.0D)){
                 col++;
             } else if (NUtils.ISNOTEQUAL(this.result.getLeft().getElement(row, col), 1.0D)){
--- src/mtrx/methods/GaussJordan.java
+++ src/mtrx/methods/GaussJordan.java
@@ -25,14 +25,17 @@ public class GaussJordan implements GaussMethod {
             if (this.result.isAllXinCol(j, 0.0D) && (!this.result.isAllXinCol(j+1, 0.0D))){
                 col = j+1;
                 break;
+            } else if (!this.result.isAllXinCol(j, 0.0D)){
+                col = j;
+                break;
             }
         }
         if (col > this.result.getLeft().getColCount() || colZero == this.result.getLeft().getColCount()){
             this.hasSolution = false;
             return;
         }
-
         while (true){
+            System.out.println("GJ");
             if (NUtils.ISEQUAL(this.result.getLeft().getElement(row, col), 0.0D)){
                 col++;
             } else if (NUtils.ISNOTEQUAL(this.result.getLeft().getElement(row, col), 1.0D)){
